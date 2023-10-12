5aa4eb2c9510493b52385cfcb1a01f8bb1b9e463
veejask-41 <sajeevk2001@gmail.com>
2023-10-12 15:18:03 +0530

    created get and post requests to db in backend

--- server/index.js
+++ server/index.js
@@ -1,13 +1,42 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const app = express();
+app.use(express.json());
 
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "KSajeev@5",
   database: "online-book-library",
+});
+
+app.get("/", (req, res) => {
+  res.json("this is home route in backend");
+});
+
+app.get("/books", (req, res) => {
+  const query = "select * from books";
+  db.query(query, (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.post("/books", (req, res) => {
+  const query =
+    "insert into books (`title`,`description`,`cover`) values (?,?,?)";
+  const values = [req.body.title, req.body.description, req.body.cover];
+  db.query(query, values, (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json("book successfully added");
+    }
+  });
 });
 
 app.listen(1234, () => {
