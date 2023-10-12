97fe27971bf5c56016273d74bb29e98082ac7ab4
veejask-41 <sajeevk2001@gmail.com>
2023-10-12 15:49:15 +0530

    added boilerplate code for frontend

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/add" Component={Add} />
+          <Route path="/update" Component={Update} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
